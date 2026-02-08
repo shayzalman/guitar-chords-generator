803c8c7b201ac1f6862f888fa8138ef63c357b5e
Shay Zalman <shayzalman@gmail.com>
2026-02-08 15:39:28 +0200

    Allow CORS for additional local development origin at localhost:3001

--- server/app.py
+++ server/app.py
@@ -24,7 +24,7 @@ app.mount("/downloads", StaticFiles(directory=DOWNLOADS_DIR), name="downloads")
 # Allow CORS for local development
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:3001"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
